dada32299c5e807f2692b8034962272c07db6396
Dora <trista.fly@gmail.com>
2022-12-09 21:42:17 +0800

    改版

--- js-preview-01/all.js
+++ js-preview-01/all.js
@@ -9,17 +9,20 @@ const inputData = document.querySelector("#password");
 const btnclick = document.querySelector(".area-btn");
 const btnreset = document.querySelector(".reset");
 
-const  icon = document.querySelector(".icon");
+const icon = document.querySelector(".icon");
 
 const showrange = document.querySelector(".show-range");
 const over = document.querySelector(".over");
+const count = document.querySelector(".count");
 
 
 const mainbtn = document.querySelector(".main-btn");
 
 let inputStr = "";
 let num = "";
-
+let errorCount = 0;
+let rangeCount = 0;
+let blankCount = 0;
 
 
 // 亂數的區間
@@ -28,9 +31,9 @@ let init_max = 100;
 let randomInt = getRandomInt(init_min, init_max);
 
 // 設定頁面顯示區間範圍
-nowRange(init_min,init_max);
+nowRange(init_min, init_max);
 
-function nowRange(min,max){
+function nowRange(min, max) {
 
     Range_min.innerText = min;
     Range_max.innerText = max;
@@ -39,14 +42,14 @@ function nowRange(min,max){
 
 
 
-console.log("終極密碼: "+randomInt);
+console.log("終極密碼: " + randomInt);
 
 
 // 清空輸入值
-function resetInput(){
+function resetInput() {
     inputStr = "";
     inputData.value = inputStr;
-    
+
 }
 
 // 輸入數字鍵盤的出發
@@ -60,11 +63,11 @@ btnclick.addEventListener("click", function (e) {
             console.log("我點到送出");
             inputStr = inputData.value;
             checkInputData(inputStr);
-        } else if(e.target.getAttribute("class") == "btn") {
+        } else if (e.target.getAttribute("class") == "btn") {
             console.log("我點到數字");
             num = e.target.getAttribute("data-num");
             renderInputData(num);
-        } else{
+        } else {
             window.location.reload();
         }
     }
@@ -81,58 +84,211 @@ function renderInputData(num) {
     }
 }
 
+function render_showrange() {
+    showrange.innerHTML = str_showrange;
+}
+function render_count() {
+    count.innerText = str_count;
+}
+function render_over() {
+    over.innerText = str_over;
+}
+function render_icon() {
+    icon.innerHTML = str_icon;
+}
+function render_mainbtn() {
+    mainbtn.innerHTML = str_mainbtn;
+}
+
+
+let str_showrange = "";
+let str_count = "";
+let str_over = "";
+let str_icon = ""
+let str_mainbtn = "";
+
+
+
+
+
 function checkInputData(inputStr) {
     console.log("inputStr:" + inputStr);
     inputValue = parseInt(inputStr);
 
-    // 密碼跟猜的數字相同
-    if(randomInt === inputValue){
+    errorCount++;
+    console.log(errorCount);
+
+    // 密碼跟猜的數字
+    if (inputStr === "") {
+
+        str_icon = `<img class="icon" src="https://spy-family.net/assets/img/special/anya/01.png" alt="">`;
+
+        errorCount--;
+        blankCount++;
+        console.log(blankCount);
+        if (errorCount === 0 ) {
+
+            str_icon = `<img class="icon" src="https://spy-family.net/assets/img/special/anya/02.png" alt="">`;
+
+            str_count = `這次不算,好像怪怪的~?`;
+            str_over = `送出前要輸入數字呀...==V==`;
+            blankCount--;
+
+        } else {
+            switch (blankCount) {
+                case 1:
+                    str_icon = `<img class="icon" src="https://spy-family.net/assets/img/special/anya/02.png" alt="">`;
+                    str_count = `這次不算,你只猜${errorCount}次`;
+                    str_over = `別忘了輸入數字再送出啊...`;
+                    break;
+                case 2:
+                    str_icon = `<img class="icon" src="https://spy-family.net/assets/img/special/anya/02.png" alt="">`;
+                    str_count = `這次不算,你只猜${errorCount}次`;
+                    str_over = `還是空白耶~...==V==`;
+                    break;
+
+                case 3:
+                    str_icon = `<img class="icon" src="https://spy-family.net/assets/img/special/anya/02.png" alt="">`;
+                    str_count = `這次不算,你只猜${errorCount}次`;
+                    str_over = `一直送我空白~`;
+                    break;
+
+   
+                default:
+                    str_icon = `<img class="icon" src="https://spy-family.net/assets/img/special/anya/17.png" alt="">`;
+                    str_count = `你空白太多次了~`;
+                    str_over = `...我要送你去別的地方~(掰)`;
+
+                    setTimeout("location.href='https://karta060925.github.io/ultimatePassword/index.html'",2000);
+                   
+                    break;
+
+            }
+
+        }
+
+
+        render_icon();
+        render_count();
+        render_over();
+
+    } else if (randomInt === inputValue) {
+
         console.log("你答對了");
-        showrange.innerHTML=`<p class="win">恭喜恭喜恭喜恭喜<p>`;
-        over.innerText="你猜對了^^";
-        icon.innerHTML = `<img class="icon" src="https://spy-family.net/assets/img/special/anya/20.png" alt="">`;
-        mainbtn.innerHTML = `<button class="again">again</button>`
-    }else if(inputValue > init_max || inputValue< init_min){
-        
+
+        str_showrange = `<p class="win">恭喜恭喜恭喜恭喜<p>`;
+
+        str_icon = `<img class="icon" src="https://spy-family.net/assets/img/special/anya/20.png" alt="">`;
+
+        str_count = `你總共猜了${errorCount}次`;
+        str_over = `總算猜對了^^`;
+
+        str_mainbtn = `<button class="again">again</button>`
+
+        render_showrange();
+        render_icon();
+        render_count();
+        render_over();
+        render_mainbtn();
+
+
+    } else if (inputValue > init_max || inputValue < init_min) {
+
         // 密碼 超過 最大值 或  密碼 小於 最小值
         console.log("不在區間範圍內");
-        icon.innerHTML = `<img class="icon" src="https://spy-family.net/assets/img/special/anya/07.png" alt="">`;
-        over.innerText="你幹嘛猜不在區間範圍的數字~";
+
+        str_icon = `<img class="icon" src="https://spy-family.net/assets/img/special/anya/07.png" alt="">`;
+
+       
+
+        if (errorCount > 3) {
+            str_count = `你還是沒猜對~猜第${errorCount}次了~`;
+            str_over = `超出區間範圍的數字啦(火爆)!!`;
+        } else {
+            str_count = `你沒猜對~猜第${errorCount}次了~`;
+            str_over = `超出區間範圍的數字了唷~`;
+        }
+
+        render_icon();
+        render_count();
+        render_over();
+
 
         // 清空輸入值
         resetInput();
 
-    }else{
+    } else {
 
-        
+
         // 密碼 超過 亂數 , 且 密碼 小於 區間最大值 
-        if(inputValue > randomInt && inputValue < init_max){
-            icon.innerHTML = `<img class="icon" src="https://spy-family.net/assets/img/special/anya/11.png" alt="">`;
+        if (inputValue > randomInt && inputValue < init_max) {
 
-            console.log("進入")
+            str_icon = `<img class="icon" src="https://spy-family.net/assets/img/special/anya/11.png" alt="">`;
+
             // 區間最大值  改為 本次輸入的密碼
             init_max = inputValue;
-            nowRange(init_min,init_max);
-            over.innerText="還沒猜對啦~";
-            resetInput();
-        }else if(inputValue < randomInt && inputValue > init_min){
+            nowRange(init_min, init_max);
 
-            icon.innerHTML = `<img class="icon" src="https://spy-family.net/assets/img/special/anya/12.png" alt="">`;
+           
+
+            if (errorCount > 3) {
+                str_count = `你還是沒猜對~你猜第${errorCount}次了`;
+                str_over = `認真點好嗎~?`;
+            } else {
+                str_count = `你沒猜對~你猜第${errorCount}次了`;
+                str_over = `親愛的加油~!!`;
+            }
+
+
+            render_icon();
+            render_count();
+            render_over();
+
+            resetInput();
+
+        } else if (inputValue < randomInt && inputValue > init_min) {
+
+            str_icon = `<img class="icon" src="https://spy-family.net/assets/img/special/anya/12.png" alt="">`;
 
             // 密碼 小於 亂數  且 密碼 大於 區間最小值 
 
             // 區間最小值  改為 本次輸入的密碼
             init_min = inputValue;
-            nowRange(init_min,init_max);
-            over.innerText="還沒猜對啦~";
+            nowRange(init_min, init_max);
+
+            
+
+            if (errorCount > 3) {
+                str_count = `你還是沒猜對~你猜第${errorCount}次了`;
+                str_over = `真的要認真~?`;
+            } else {
+                str_count = `你沒猜對~你猜第${errorCount}次了`;
+                str_over = `給你打打氣~!!`;
+            }
+
+
+            render_icon();
+            render_count();
+            render_over();
+
             resetInput();
-        }else{
-            icon.innerHTML = `<img class="icon" src="https://spy-family.net/assets/img/special/anya/04.png" alt="">`;
-            over.innerText="這不是區間範圍內的數字...";
+        } else {
+            str_icon = `<img class="icon" src="https://spy-family.net/assets/img/special/anya/04.png" alt="">`;
+            if (errorCount > 3) {
+                str_count = `你還是沒猜對~你猜第${errorCount}次了`;
+            str_over = `確認一下輸入區間可以嗎?`;
+            } else {
+                str_count = `你沒猜對~你猜第${errorCount}次了`;
+            str_over = `確認一下輸入區間呀...`;
+            }
+            
+
+            render_icon();
+            render_count();
+            render_over();
+
             resetInput();
         }
-        
-        
 
     }
 
@@ -143,12 +299,13 @@ function getRandomInt(min, max) {
     // 函式會回傳大於等於所給數字的最小整數
     console.log(min);
     min = Math.ceil(min);
-    console.log("最小值: "+min);
+    console.log("最小值: " + min);
     // 函式會回傳小於等於所給數字的最大整數 
     console.log(max);
     max = Math.floor(max);
-    console.log("最大值: "+max);
+    console.log("最大值: " + max);
 
     // 取最小值
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
 }
+
